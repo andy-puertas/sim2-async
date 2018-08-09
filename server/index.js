ae0fd76dd233c8a7989a60e5b455f9d3b744cf46
Andy Puertas <andy.puertas08@gmail.com>
2018-08-09 14:46:58 -0600

    added assets, working on backend

---
+++ server/index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+const express = require('express')
+    , massive = require('massive')
+    , bodyParser = require('body-parser')
+    , cors = require('cors')
+    , session = require('express-session')
+    , ctrl = require('./controller');
+
+const app = express();
+
+app.use( bodyParser.json() );
+app.use( cors() );
+
+const {
+  SERVER_PORT,
+  CONNECTION_STRING,
+  SESSION_SECRET
+} = process.env;
+
+
+
+app.listen(SERVER_PORT, () => {
+  console.log('Listening on port:', SERVER_PORT)
+});
+
+
+massive(CONNECTION_STRING)
+  .then(dbInstance => {
+    console.log('Database is connected');
+    app.set('db', dbInstance);
+  })
+  .catch(err => {
+    console.log(err);
+    app.set('db', dbInstance);
+  })
